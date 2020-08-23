d0b9abd102e62bda93d744dcc33a343bb7fdf131
mhdzam <intell_progr_1991@hotmail.com>
2020-08-24 01:08:03 +0400

    first commit

---
+++ BackendServices/Controllers/UsersController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackendServices.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.AccessControl;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace BackendServices.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly BookStoreContext _context;
+        private readonly JWTSettings _JWTToken;
+
+        public UsersController(BookStoreContext context, IOptions<JWTSettings> JwtToken)
+        {
+            _context = context;
+            _JWTToken = JwtToken.Value;
+        }
+
+        // POST: api/Users
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserWithToken>> Login([FromBody] User user)
+        {
+            user = await _context.User.Include(u => u.Role)
+                                        .Where(u => u.EmailAddress == user.EmailAddress
+                                                && u.Password == user.Password).
+                                                FirstOrDefaultAsync();
+
+            UserWithToken userWithToken = new UserWithToken(user);
+
+          
+         
+            if (userWithToken == null)
+            {
+                return NotFound();
+            }
+
+            //sing your tokrn here ...
+            var tokenhandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_JWTToken.SecretKey);
+            var tokeDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,user.EmailAddress)
+                }),
+                Expires = DateTime.Now.AddMonths(6),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                 SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenhandler.CreateToken(tokeDescriptor);
+            userWithToken.Token = tokenhandler.WriteToken(token);
+
+            return userWithToken;
+        }
+
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("GetUser")]
+        public async Task<ActionResult<User>> GetUser()
+        {
+            string emailaddress = HttpContext.User.Identity.Name;
+            var user = await _context.User.Where(usr => usr.EmailAddress == emailaddress).FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserId }, user);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.UserId == id);
+        }
+    }
+}
---
+++ BackendServices/Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,69 @@
+﻿using BackendServices.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace BackendServices.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private BookStoreContext _ctx;
+
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            BookStoreContext ctx): base(options,logger,encoder,clock)
+        {
+            _ctx = ctx;
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Authorization header was not found");
+
+            try
+            {
+      
+          var authAutherizationheadervalue = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+
+            var bytes = Convert.FromBase64String(authAutherizationheadervalue.Parameter);
+
+            string[] credentials = Encoding.UTF8.GetString(bytes).Split(" ");
+            string email = credentials[0];
+            string password = credentials[1];
+
+            User _user = _ctx.User.Where(usr => usr.Password == password && usr.EmailAddress == email).FirstOrDefault();
+
+            if (_user == null)
+            {
+                return AuthenticateResult.Fail("Wrong user name or password");
+            }
+            else
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, _user.EmailAddress) };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principle = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principle, Scheme.Name);
+
+                return AuthenticateResult.Success(ticket);
+            }
+
+        }
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail("Error has Occured");
+            } 
+        }
+    }
+}
